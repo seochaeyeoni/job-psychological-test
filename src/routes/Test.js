2d67db503786ad656f0ebabbf3740c231273c9cd
채연 서 <alice_98@naver.com>
2021-08-23 22:33:55 +0900

    Feat: test 뷰 보여주기 route page 방식으로

--- src/routes/Test.js
+++ src/routes/Test.js
@@ -1,15 +1,46 @@
 import { useRecoilValue } from 'recoil';
 import { questionState } from '../atom';
+import Button from '../components/Button';
+import ProgressBar from '../components/ProgressBar';
+import Question from '../components/Question';
+import { useHistory } from 'react-router-dom';
 
-const Test = ({match}) => {
-    const questionValue = useRecoilValue(questionState);
-    console.log(questionValue)
+const Test = ({ match }) => {
+  const history = useHistory();
+  const questionValue = useRecoilValue(questionState);
+  console.log(questionValue);
+  const page = Number(match.params.page);
+  const progress = (100 / 6) * page;
 
+  const radioChange = () => {};
+
+  const nextPage = () => {
+    history.push(`/test/${page + 1}`);
+  };
+
+  const questions = () => {
     return (
-        <>
-        <h1>{match.params.page}</h1>
-        </>
-    )
-}
+      <>
+        {questionValue.slice((page-1)*5,5*page).map((data, index) => (
+          <Question
+            Question={data.question}
+            value1={data.answer01}
+            value2={data.answer02}
+            onClick={radioChange}
+          />
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <h1>{match.params.page}</h1>
+      <ProgressBar percent={progress} />
+      {questions()}
+      <Button name="다음" disabled={false} onClick={nextPage} />
+    </>
+  );
+};
 
 export default Test;
