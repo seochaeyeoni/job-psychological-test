951d40f3eb8dcd44f2316f23798c205fa440da8b
채연 서 <alice_98@naver.com>
2021-08-27 22:15:55 +0900

    Fix: gender issue 해결

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -40,8 +40,9 @@ const Home = () => {
     setUser(temp);
   };
 
-  const radioChange = value => {
+  const radioChange = (group, value) => {
     const temp = { ...user };
+    console.log(value)
     temp.gender = value;
     setUser(temp);
   };
@@ -54,8 +55,8 @@ const Home = () => {
         <h5>이름</h5>
         <Input onChange={inputChange} />
         <h5>성별</h5>
-        <RadioButton value="남자" group="gender" onChange={radioChange} />
-        <RadioButton value="여자" group="gender" onChange={radioChange} />
+        <RadioButton value="남자" group="gender" hiddenValue="남자" onChange={radioChange} />
+        <RadioButton value="여자" group="gender" hiddenValue="여자" onChange={radioChange} />
         <Button name="검사시작" disabled={status} onClick={checkUser} />
       </Inner>
     </Outer>
