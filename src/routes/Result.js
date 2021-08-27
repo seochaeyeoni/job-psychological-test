5a46696ad55e6f156ecdfbe8fe7cfcb44c94afb7
채연 서 <alice_98@naver.com>
2021-08-27 13:17:34 +0900

    Feat: parseJobAndMajor

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -17,20 +17,15 @@ const Finish = () => {
   const resetMajor = useResetRecoilState(userState);
   const history = useHistory();
   const gotoHome = () => {
-    resetUser()
-    resetWonScore()
-    resetJob()
-    resetMajor()
-    console.log(userValue, wonScoreValue, jobValue, majorValue)
+    resetUser();
+    resetWonScore();
+    resetJob();
+    resetMajor();
+    console.log(userValue, wonScoreValue, jobValue, majorValue);
     history.push('/');
   };
 
   /* 
-  - Chart Component
-  Chart.js Bar Chart
-  ex)
-  label = ['자율성', '창의성', '보수']
-  data = ['5', '5', '4']
   - Table Component
   ex1) only col
   col = ['이름', '성별', '검사일']
@@ -45,10 +40,33 @@ const Finish = () => {
   majorNames는 0부터 매칭
   */
 
+  const parseJobMajor = (value, names) => {
+    let dict = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [] };
+    let row = [];
+    let data = [];
+    value.forEach(elem => {
+      let temp = dict[elem[2]];
+      temp.push(elem.slice(0, 2));
+      dict[elem[2]] = temp;
+    });
+    for (const [key, value] of Object.entries(dict)) {
+      console.log(key, value);
+      if (value.length !== 0) {
+        row.push(names[key]);
+        data.push(value);
+      }
+    }
+    console.log(row);
+    console.log(data);
+    return [row, data];
+  };
+
   return (
     <>
       <h1>직업가치관검사 결과표</h1>
-      <UserTable userValue={userValue}/>
+      <p>{parseJobMajor(jobValue, educationLevelNames)}</p>
+      <p>{parseJobMajor(majorValue, majorNames)}</p>
+      <UserTable userValue={userValue} />
       <p>
         직업가치관이란 직업을 선택할 때 영향을 끼치는 자신만의 믿음과 신념입니다. 따라서 여러분의
         직업생활과 관련하여 포기하지 않는 무게중심의 역할을 한다고 볼 수 있습니다. 직업가치관검사는
@@ -57,7 +75,7 @@ const Finish = () => {
         제공합니다.
       </p>
       <h3>직업가치관 결과</h3>
-      <Chart labels={interpretationNames} datas={Object.values(wonScoreValue).map(Number)}/>
+      <Chart labels={interpretationNames} datas={Object.values(wonScoreValue).map(Number)} />
       <h3>가치관과 관련이 높은 직업</h3>
       <h4>종사자 평균 학력별</h4>
       <h4>종사자 평균 전공별</h4>
